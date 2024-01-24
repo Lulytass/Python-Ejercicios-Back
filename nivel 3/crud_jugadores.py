a1fb22587b9a22a9aa250abb846ada96ee8ed5e4
Lulytass <88911233+Lulytass@users.noreply.github.com>
2024-01-24 14:37:24 -0300

    Add files via upload

--- nivel 3/crud_jugadores.py
+++ nivel 3/crud_jugadores.py
@@ -16,3 +16,180 @@
 # Utilize the comparison feature to analyze and contrast the characteristics of two players.
 # Visualize Characteristics:
 # Display the statistical and physical attributes of each player for a comprehensive overview.
+
+class Crud_equipo():
+    def __init__(self):
+        self.jugadores = {
+            "8": {
+                'nombre': 'Bruno Fernandes', 'posicion': 'Centro Campista', 'edad': 29,
+              'altura': 1.79, 'goles': 5, 'puntos asistencia': 9
+            },
+            "11": {
+                'nombre': 'Rasmus Hojlund', 'posicion': 'Delantero', 'edad': 20, 'altura': 1.91,
+               'goles': 12, 'puntos asistencia': 2 
+            },
+            "5": {
+                'nombre': 'Harry Maguire', 'posicion': 'Defensa', 'edad': 30, 'altura': 1.94,
+              'goles': 1, 'puntos asistencia': 1
+            },
+            "17": {
+                'nombre': 'Alejandro Garnacho', 'posicion': 'Centro Campista', 'edad': 19,
+               'altura': 1.80, 'goles': 8, 'puntos asistencia': 8 
+            },
+            "7": {
+                'nombre': 'Mason Mount', 'numero camiseta': 7, 'posicion': 'Centro Campista', 'edad': 25, 'altura': 1.81,
+              'goles': 2, 'puntos asistencia': 4
+            },
+        }
+        self.menu = {
+            "Ver jugadores": "jugadores",
+            "Comparar dos jugadores": "comparar",
+            "Visualizar características": "caracteristicas",
+            "Crear registro": "crear",
+            "Dar de baja jugador": "baja",
+            "Editar jugador": "editar",
+            "Salir del sistema": "cerrar"
+        }
+
+    def mostrar_menu(self):
+        print("----Bienvenido----\n")
+        for i, opciones in enumerate(self.menu.keys(), start=1):
+            print(f"{i}. {opciones}")
+
+    def obtener_opcion(self, estruc, mensaje, retorno_clave=False):
+        while True:
+            try:
+                opcion = int(input(f"\n{mensaje} (1-{len(estruc)}): "))
+                if 1 <= opcion <= len(estruc):
+                    if retorno_clave:
+                        str_opcion = list(estruc.values())[opcion - 1]
+                        return str_opcion
+                    else:
+                        return opcion
+                else:
+                    print("\nSelección inválida. Asegúrese de elegir una dentro del rango.")
+            except ValueError:
+                print("\nPor favor, ingrese un número válido.")
+
+    def mostrar_jugadores(self):
+        print("----Jugadores Disponibles----\n")
+        for i, datos in enumerate(self.jugadores.values(), start=1):
+            print(f"{i}. {datos['nombre']}")
+
+    def seleccionar_jugadores(self, estruc):
+        opcion1 = self.obtener_opcion(estruc, "Seleccione el primer jugador", False)
+        opcion2 = self.obtener_opcion(estruc, "Seleccione el segundo jugador", False)
+
+        return opcion1, opcion2
+
+    def mostrar_jugador(self, jugador):
+        camiseta = list(self.jugadores.keys())[jugador - 1]
+        jugador_seleccionado = list(self.jugadores.values())[jugador - 1]
+        print(f"\nCamiseta Numero: {camiseta}\nPosición: {jugador_seleccionado['posicion']}\nNombre: {jugador_seleccionado['nombre']}\nGoles: {jugador_seleccionado['goles']}\nAsistencias: {jugador_seleccionado['puntos asistencia']}\nEdad: {jugador_seleccionado['edad']}\nAltura: {jugador_seleccionado['altura']}")
+   
+    def crear_registro(self):
+        print("----Crear Nuevo Jugador----\n")
+        while True:
+            try:
+                nombre = input("Nombre del jugador: ")
+                posicion = input("Posición: ")
+                edad = int(input("Edad: "))
+                altura = float(input("Altura: "))
+                goles = int(input("Goles: "))
+                asistencias = int(input("Asistencias: "))
+                numero_camiseta = int(input("Camiseta: "))
+                # crea un numero de camiseta unico
+                if str(numero_camiseta) in self.jugadores.keys():
+                    input("Ese numero de camiseta ya esta en uso")
+                    continue
+                else:
+                    nuevo_jugador = {
+                        'nombre': nombre,
+                        'posicion': posicion,
+                        'edad': edad,
+                        'altura': altura,
+                        'goles': goles,
+                        'puntos asistencia': asistencias,
+                        'numero camiseta': numero_camiseta
+                    }
+
+                    self.jugadores[str(numero_camiseta)] = nuevo_jugador
+                    print(f"\n¡Jugador {nombre} agregado al sistema con éxito!\n")
+                    break
+            except ValueError:
+                print("\nPor favor, ingrese un número válido.")
+
+    def actualizar_jugador(self):
+        self.mostrar_jugadores()
+        jugador_a_actualizar = self.obtener_opcion(self.jugadores.keys(), "Seleccione el jugador que desea actualizar", False)
+
+        if 1 <= jugador_a_actualizar <= len(self.jugadores):
+            clave_jugador = list(self.jugadores.keys())[jugador_a_actualizar - 1]
+            jugador_actualizado = self.jugadores[clave_jugador]
+
+            print(f"\n----Actualizar Jugador {clave_jugador}----")
+            print("Deje en blanco si no desea realizar cambios.")
+
+            nombre = input(f"Nombre actual ({jugador_actualizado['nombre']}): ") or jugador_actualizado['nombre']
+            posicion = input(f"Posición actual ({jugador_actualizado['posicion']}): ") or jugador_actualizado['posicion']
+            edad = input(f"Edad actual ({jugador_actualizado['edad']}): ") or jugador_actualizado['edad']
+            altura = input(f"Altura actual ({jugador_actualizado['altura']}): ") or jugador_actualizado['altura']
+            goles = input(f"Goles actual ({jugador_actualizado['goles']}): ") or jugador_actualizado['goles']
+            asistencias = input(f"Asistencias actual ({jugador_actualizado['puntos asistencia']}): ") or jugador_actualizado['puntos asistencia']
+
+            self.jugadores[clave_jugador] = {
+                'nombre': nombre,
+                'posicion': posicion,
+                'edad': int(edad),
+                'altura': float(altura),
+                'goles': int(goles),
+                'puntos asistencia': int(asistencias)
+            }
+
+            print(f"\n¡Jugador {nombre} actualizado con éxito!\n")
+        else:
+            print("\nSelección inválida. Asegúrese de elegir una dentro del rango.")
+
+    def dar_de_baja_jugador(self):
+        self.mostrar_jugadores()
+        jugador_a_eliminar = self.obtener_opcion(self.jugadores.keys(), "Seleccione el jugador que desea dar de baja", False)
+
+        if 1 <= jugador_a_eliminar <= len(self.jugadores):
+            clave_jugador = list(self.jugadores.keys())[jugador_a_eliminar - 1]
+            jugador_eliminado = self.jugadores.pop(clave_jugador)
+            print(f"\n¡Jugador {jugador_eliminado['nombre']} dado de baja con éxito!\n")
+        else:
+            print("\nSelección inválida. Asegúrese de elegir una dentro del rango.")
+
+    def programa_principal(self):
+        while True:
+            self.mostrar_menu()
+            opcion_menu = self.obtener_opcion(self.menu, "Seleccione una de las opciones",True)
+            if opcion_menu == "jugadores":
+                self.mostrar_jugadores()
+                opcion_jugador = self.obtener_opcion(self.menu, "Seleccione un jugador",False)
+                self.mostrar_jugador(opcion_jugador)                
+            elif opcion_menu == "comparar":
+                self.mostrar_jugadores()
+                opcion1, opcion2 = self.seleccionar_jugadores(self.jugadores)
+                print("\nCaracterísticas del primer jugador\n")
+                self.mostrar_jugador(opcion1)
+                print("\nCaracterísticas del segundo jugador\n")
+                self.mostrar_jugador(opcion2)
+            elif opcion_menu == "caracteristicas":
+                self.mostrar_jugadores()
+                opcion_jugador = self.obtener_opcion(self.menu, "Seleccione un jugador",False)
+                self.mostrar_jugador(opcion_jugador) 
+            elif opcion_menu == "crear":
+                self.crear_registro()
+            elif opcion_menu == "editar":
+                self.actualizar_jugador()
+            elif opcion_menu == "baja":
+                self.dar_de_baja_jugador()
+            else:
+                break
+
+
+
+prueba = Crud_equipo()
+prueba.programa_principal()
--- nivel 3/venta_entradas.py
+++ nivel 3/venta_entradas.py
@@ -27,3 +27,239 @@
 # Generate and issue tickets.
 # Remaining Seat Display:
 # The system should display the number of available seats after each purchase.
+
+class Asientos:
+    def __init__(self):
+        self.total_asientos = 74310
+        self.precios_asientos = {'vip box': 1000, 'vip': 500, 'general': 90}
+        self.asientos = {'vip box': list(range(1, 3716)), 'vip': list(range(3716, 14863)), 'general': list(range(14863, 74311))}
+
+    def comprobar_existencia(self, claseAsiento, numeroAsiento):
+        return numeroAsiento in self.asientos[claseAsiento]
+    
+    def tipos_asientos(self):
+        claves = list(self.asientos.keys())
+        for i, (tipo, asiento) in enumerate(self.asientos.items(), start=1):
+            print(f"{i}. {tipo}: del asiento {asiento[0]} al {asiento[len(asiento)-1]}")
+        while True:
+            try:
+                seleccion = int(input(f"Seleccione un tipo de asiento (1-{len(self.asientos.items())}): "))
+                if 1 <= seleccion <= len(self.asientos.items()):
+                    return claves[seleccion-1]
+                else:
+                    print("\nSelección inválida. Asegúrese de elegir una dentro del rango.")
+            except ValueError:
+                print("\nPor favor, ingrese un valor válido.")
+
+    def elegir_numero(self, clave):
+        while True:
+            try: 
+                eleccion = int(input(f"Seleccione un asiento del {self.asientos[clave][0]} al {self.asientos[clave][len(self.asientos[clave])-1]}: "))
+                if self.asientos[clave][0] <= eleccion <= self.asientos[clave][len(self.asientos[clave])-1]:
+                    return eleccion
+                else:
+                    print("Ingrese un numero dentro del rango señalado")
+            except ValueError:
+                print("\nPor favor, ingrese un valor válido.")
+
+    def ocupar_asiento(self, clase, asiento): 
+        try:
+            self.asientos[clase].remove(asiento)
+            print("El asiento se reservo correctamente")
+            self.total_asientos -= 1
+            return True
+        except ValueError:
+            print("No se pudo realizar la reserva")
+            return False
+
+    def descuento_miembro(self, miembro, clase):
+        if miembro == True:
+            precio = self.precios_asientos[clase] * 0,15
+            print(self.precios_asientos[clase])
+            print("El precio de las entradas cuentan con un 15% de descuento por ser socio del club")
+        else:
+            precio = self.precios_asientos[clase]
+        return precio
+    
+    def asiento_cercano(self, clase, asiento):
+        # Buscar hacia adelante
+        for i in range(asiento, self.asientos[clase][-1] + 1):
+            if self.comprobar_existencia(clase, i):
+                return i
+
+        # Buscar hacia atrás
+        for i in range(asiento - 1, self.asientos[clase][0] - 1, -1):
+            if self.comprobar_existencia(clase, i):
+                return i
+
+        # Si no se encuentra ningún asiento disponible en la misma clase
+        return None
+
+
+class Usuario:
+    def __init__(self):
+        self.usuarios = {}
+        self.attempts = 3
+        self.Usuario_membership = False
+
+    def pedir_datos_nuevo_usuario(self, es_miembro):
+        # Solicitar nombre de usuario
+        while True:
+            nombre = input("Ingrese un nombre de usuario: ").strip()
+
+            # Verificar si el usuario ya existe
+            if nombre in self.usuarios:
+                print("El usuario ya existe. Por favor, elija otro nombre de usuario.")
+                continue
+            else:
+                break
+
+        # Solicitar contraseña
+        while True:
+            contrasenia = input("Ingrese una contraseña (mínimo 6 caracteres): ").strip()
+
+            # Validar longitud de contraseña
+            if len(contrasenia) < 6:
+                print("La contraseña debe tener al menos 6 caracteres.")
+            else:
+                break  # Salir del bucle si la contraseña es válida
+
+        # Agregar usuario al diccionario
+        self.usuarios[nombre] = {
+            "contrasenia": contrasenia,
+            "miembro": es_miembro
+        }
+        print("La cuanta fue creada exitosamente.")
+        
+
+    def crear_usuario(self):
+        while True:
+            respuesta = input("¿Es miembro? (Y/N): ").strip().upper()
+
+            if respuesta == "Y":
+                es_miembro = True
+                break
+            elif respuesta == "N":
+                es_miembro = False
+                break
+            else:
+                print("Respuesta inválida. Por favor, ingrese 'Y' para Sí o 'N' para No.")
+
+        self.pedir_datos_nuevo_usuario(es_miembro)
+
+    def login(self):
+        while self.attempts > 0:
+            nombre = input("Ingrese su nombre de usuario: ").strip()
+            if nombre in self.usuarios:
+                while True:
+                    contrasenia = input("Ingrese su contraseña: ").strip()
+                    if self.usuarios[nombre]["contrasenia"] == contrasenia:
+                        return True, self.usuarios[nombre]["miembro"]
+                    else:
+                        print("Contraseña incorrecta. Inténtelo nuevamente.")
+                        self.attempts -= 1
+                        print(f"Contraseña incorrecta. Intentos restantes: {self.attempts}")
+                        if self.attempts == 0:
+                            print("Cuenta bloqueada. Por favor, contacte al soporte.")
+                            return False
+            else:
+                print("No existe un usuario con ese nombre.")
+                return False
+
+    def menu_inicio(self):
+        print("------Bienvenido------")
+        print("1. Crear Usuario")
+        print("2. Ingresar")
+
+    def leer_opcion(self):
+        
+        while True:
+            self.menu_inicio()
+            try:
+                opcion = int(input("Ingrese una opcion (1-2): "))
+                if 1 <= opcion <= 2:
+                    if opcion == 1:
+                        self.crear_usuario()
+                    elif opcion == 2:
+                        return self.login()      
+                else:
+                    print("Ingrese una opcion dentro del rango correcto")
+            except ValueError:
+                print("\nPor favor, ingrese un valor válido.")
+
+
+class Estadio:
+    def __init__(self):
+        self.entradas = Asientos()
+        self.usuario = Usuario()
+        self.entradas_vendidas = []
+
+    def menu(self):
+        print("1. Comprar entradas")
+        print("2. Salir")
+
+    def leer_opcion(self):
+        while True:
+            self.menu()
+            try:
+                opcion = int(input("Ingrese una opcion (1-2): "))
+                if 1 <= opcion <= 2:
+                    if opcion == 1:
+                        return True
+                    elif opcion == 2:
+                        return False   
+                else:
+                    print("Ingrese una opcion dentro del rango correcto")
+            except ValueError:
+                print("\nPor favor, ingrese un valor válido.")
+
+
+    def programa_principal(self):
+        asientos_disponibles = 3
+        login, miembro = self.usuario.leer_opcion()
+        if miembro == True:
+            asientos_disponibles = 10
+        while asientos_disponibles > 0 and login:
+            if self.leer_opcion():
+                clave = self.entradas.tipos_asientos()
+                valor = self.entradas.elegir_numero(clave)
+                if self.entradas.comprobar_existencia(clave, valor):
+                    precio = self.entradas.descuento_miembro(miembro, clave)
+                    print(f"El precio por {clave} nro. {valor} es {precio}")
+                    respuesta = input("Ingrese Y para confirmar o cual otra tecla para cancelar: ").strip().upper()
+                    if respuesta == "Y":
+                        self.entradas.ocupar_asiento(clave, valor)
+                        self.entradas_vendidas.append({
+                            "clase": clave,
+                            "asiento": valor,
+                            "precio": precio
+                        })
+                        asientos_disponibles -= 1
+            else:
+                break
+        if asientos_disponibles < 1:
+            print("Llego al limite de entradas que puede adquirir")
+        print("-----Detalle de compra-----")
+        for i, datos in enumerate(self.entradas_vendidas, start=1):
+            print(f"{i}. Ubicación: {datos['clase']} Asiento: {datos['asiento']} Precio {datos['precio']}")
+        
+    
+
+
+
+                
+est = Estadio()
+est.programa_principal()
+
+
+# ent = Asientos()
+# ent.elegir_numero("vip")
+# ent.ocupar_asiento("vip", 3800)
+# ent.mostrar_tipos_asientos()
+
+# # Uso de la clase Usuario
+                
+# usuario = Usuario()
+# sesion_iniciada, miembro = usuario.leer_opcion()
+# print(sesion_iniciada)
+# print(miembro)
